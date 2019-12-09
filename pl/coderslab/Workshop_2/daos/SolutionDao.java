572c5257b7fba19de59a63ccbeefae06d508ad7b
Gottskalksson <adam.rolirad@gmail.com>
2019-12-09 21:52:22 +0100

    Exercise + Solution + DAOs

--- pl/coderslab/Workshop_2/daos/SolutionDao.java
+++ pl/coderslab/Workshop_2/daos/SolutionDao.java
@@ -1,4 +1,107 @@
 package pl.coderslab.Workshop_2.daos;
 
+import pl.coderslab.Workshop_2.DBUtil;
+import pl.coderslab.Workshop_2.models.Solution;
+import java.sql.*;
+import java.util.Arrays;
+
 public class SolutionDao {
+
+    private static final String
+            CREATE_SOLUTION_QUERY = "INSERT INTO solutions(created, description) VALUES (?, ?)";
+    private static final String
+            READ_SOLUTION_QUERY = "SELECT * FROM solutions where id = ?";
+    private static final String
+            UPDATE_SOLUTION_QUERY = "UPDATE solutions SET created = ?, updated = ?, description = ? where id = ?";
+    private static final String
+            DELETE_SOLUTION_QUERY = "DELETE FROM solutions WHERE id = ?";
+    private static final String
+            FIND_ALL_SOLUTIONS_QUERY = "SELECT * FROM solutions";
+
+
+
+    public Solution create(Solution solution) {
+        try (Connection conn = DBUtil.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(CREATE_SOLUTION_QUERY, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, solution.getCreated());
+            statement.setString(2, solution.getDescription());
+            statement.executeUpdate();
+            ResultSet resultSet = statement.getGeneratedKeys();
+            if (resultSet.next()) {
+                solution.setId(resultSet.getInt(1));
+            }
+            return solution;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public Solution read(int userId) {
+        try (Connection conn = DBUtil.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(READ_SOLUTION_QUERY);
+            statement.setInt(1, userId);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                Solution solution = new Solution();
+                solution.setId(resultSet.getInt("id"));
+                solution.setCreated(resultSet.getString("created"));
+                solution.setUpdated(resultSet.getString("updated"));
+                solution.setDescription(resultSet.getString("description"));
+                return solution;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void update(Solution solution) {
+        try (Connection conn = DBUtil.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(UPDATE_SOLUTION_QUERY);
+            statement.setString(1, solution.getCreated());
+            statement.setString(2, solution.getUpdated());
+            statement.setString(3, solution.getDescription());
+            statement.setInt(4, solution.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(int userId) {
+        try (Connection conn = DBUtil.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(DELETE_SOLUTION_QUERY);
+            statement.setInt(1, userId);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Solution[] findAll() {
+        try (Connection conn = DBUtil.getConnection()) {
+            Solution[] solutions = new Solution[0];
+            PreparedStatement statement = conn.prepareStatement(FIND_ALL_SOLUTIONS_QUERY);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Solution solution = new Solution();
+                solution.setId(resultSet.getInt("id"));
+                solution.setCreated(resultSet.getString("created"));
+                solution.setUpdated(resultSet.getString("updated"));
+                solution.setDescription(resultSet.getString("description"));
+                solutions = addToArray(solution, solutions);
+            }
+            return solutions;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private Solution[] addToArray(Solution s, Solution[] solutions) {
+        Solution[] tmpUsers = Arrays.copyOf(solutions, solutions.length + 1);
+        tmpUsers[solutions.length] = s;
+        return tmpUsers;
+    }
 }
--- pl/coderslab/Workshop_2/models/Solution.java
+++ pl/coderslab/Workshop_2/models/Solution.java
@@ -1,4 +1,51 @@
 package pl.coderslab.Workshop_2.models;
 
 public class Solution {
+
+    private int id;
+    private String created;
+    private String updated;
+    private String description;
+
+    public Solution () {
+
+    }
+
+    public Solution(String created, String updated, String description) {
+        this.created = created;
+        this.updated = updated;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public String getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(String updated) {
+        this.updated = updated;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
